7feab37babdc86c4aadf0e013388d8654987159d
AJAY VERMA <verma.av.ajay@gmail.com>
2018-01-31 17:46:54 +0530

    logout button added on dashboard page

--- client/src/app/components/pages/DashboardPage.jsx
+++ client/src/app/components/pages/DashboardPage.jsx
@@ -1,37 +1,51 @@
 import React from "react";
 import { connect } from "react-redux";
-import { List } from "semantic-ui-react";
+import { List, Button } from "semantic-ui-react";
 
 import NoMovieBooked from "../messages/NoMovieBooked.jsx";
+import { logout } from "../../redux/auth/logout";
 
 const DashboardPage = props => {
     return (
         <div>
-            {props.user.bookings.length == 0 ? (
-                <NoMovieBooked />
-            ) : (
-                <List>
-                    {props.user.bookings.map(record => {
-                        return (
-                            <ul key={record["_id"]}>
-                                {Object.keys(record).map((v, i) => {
-                                    return (
-                                        <li key={i}>{v + ":" + record[v]}</li>
-                                    );
-                                })}
-                            </ul>
-                        );
-                    })}
-                </List>
-            )}
+            <h1>Dashboard Page</h1>
+            <div>
+                {props.isAuthenticated && (
+                    <Button primary onClick={props.logout}>
+                        Logout
+                    </Button>
+                )}
+            </div>
+            <div>
+                {props.user.bookings.length == 0 ? (
+                    <NoMovieBooked />
+                ) : (
+                    <List>
+                        {props.user.bookings.map(record => {
+                            return (
+                                <ul key={record["_id"]}>
+                                    {Object.keys(record).map((v, i) => {
+                                        return (
+                                            <li key={i}>
+                                                {v + ":" + record[v]}
+                                            </li>
+                                        );
+                                    })}
+                                </ul>
+                            );
+                        })}
+                    </List>
+                )}
+            </div>
         </div>
     );
 };
 
 function mapStateToProps(state) {
     return {
-        user: state.user
+        user: state.user,
+        isAuthenticated: !!state.user.token
     };
 }
 
-export default connect(mapStateToProps)(DashboardPage);
+export default connect(mapStateToProps, { logout })(DashboardPage);
